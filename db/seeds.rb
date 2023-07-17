1b71ecec70de9d9b0bebc79db01072e4a767a825
Thiri <thirihtet246@gmail.com>
2023-07-17 16:33:48 +0630

    Create Api End point

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+messages = ['Hello', 'Mingalarbar', 'Ni hao', 'Konnichiwa', 'Bonjour']
+
+messages.each do |message|
+    Greeting.create(message: message)
+end
