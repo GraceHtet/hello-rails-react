5657c4985cc4e601006b7f4b96d58725df15ddc5
Thiri <thirihtet246@gmail.com>
2023-07-19 08:39:55 +0630

    Create greeting component and add route

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,7 +1,5 @@
 class RootController < ApplicationController
   def index
     @greetings = Greeting.all
-    rand = Random.rand(0..@greetings.length - 1)
-    @greeting = @greetings[rand]
   end
 end
---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Greeting = () => {
+  return (
+    <div>
+      <h1>I'm greeting to you </h1>
+    </div>
+  );
+};
+
+export default Greeting;
